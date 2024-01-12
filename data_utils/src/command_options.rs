c50f1d4e5ca8f7dabe93efa491a9dead0219f58b
WeiNyn <nguyenhuy17798@gmail.com>
2024-01-12 11:51:23 +0700

    [Dev] add backfill ip

--- data_utils/src/command_options.rs
+++ data_utils/src/command_options.rs
@@ -10,6 +10,9 @@ pub struct Options {
 pub enum Subcommand {
     #[structopt(name = "parse-ip", about= "Parse IP addresses")]
     ParseIP(ParseIPArguments),
+
+    #[structopt(name = "backfill-ip", about="Backfill IP addresses")]
+    BackfillIP(BackfillIPArguments),
 }
 
 #[derive(Debug, StructOpt)]
@@ -19,6 +22,27 @@ pub struct ParseIPArguments {
 
     #[structopt(short = "o", long = "output", help = "Output file")]
     pub output: String,
+
+    #[structopt(short = "g", long = "geolib", help = "Geo library", default_value = "data/GeoLite2-City.mmdb")]
+    pub geolib: String,
+
+    #[structopt(short = "s", long = "separator", help = "Separator", default_value = ",")]
+    pub separator: String,
+}
+
+#[derive(Debug, StructOpt)]
+pub struct BackfillIPArguments {
+    #[structopt(short = "i", long = "input", help = "Input schema")]
+    pub input: String,
+
+    #[structopt(short = "o", long = "output", help = "Output schema")]
+    pub output: String,
+
+    #[structopt(short = "f", long = "from", help = "From date")]
+    pub from: String,
+
+    #[structopt(short = "t", long = "to", help = "To date")]
+    pub to: String,
 
     #[structopt(short = "g", long = "geolib", help = "Geo library", default_value = "data/GeoLite2-City.mmdb")]
     pub geolib: String,
--- data_utils/src/main.rs
+++ data_utils/src/main.rs
@@ -1,5 +1,9 @@
 use structopt::StructOpt;
+use std::path::PathBuf;
 use std::time::Instant;
+use chrono::prelude::*;
+use chrono::Duration;
+use kdam::tqdm;
 
 mod command_options;
 mod ip_parser;
@@ -15,6 +19,33 @@ fn main() {
             let now = Instant::now();
             parse_ip(&args.input, &args.output, &args.geolib, &args.separator).unwrap();
             println!("Elapsed time: {:?}", now.elapsed());
-        }
+        },
+        Subcommand::BackfillIP(args) => {
+            println!("Backfill IP");
+            let from_date = NaiveDate::parse_from_str(&args.from, "%y-%m-%d").unwrap();
+            let to_date = NaiveDate::parse_from_str(&args.to, "%y-%m-%d").unwrap();
+
+            let duration = to_date.signed_duration_since(from_date);
+            let days = duration.num_days();
+
+            println!("Backfill for {} days from {:?} to {:?}", days, from_date, to_date);
+
+            for i in tqdm!(0..days) {
+                let date = from_date + Duration::days(i);
+                let input = args.input.replace("{date}", &date.format("%y-%m-%d").to_string());
+                let output = args.output.replace("{date}", &date.format("%y-%m-%d").to_string());
+
+                let output_path = PathBuf::from(output.clone());
+                let output_folder = output_path.parent().unwrap();
+                if !output_folder.exists() {
+                    std::fs::create_dir_all(output_folder).unwrap();
+                }
+
+                println!("Input: {}", input);
+                println!("Output: {}", output);
+                parse_ip(&input, &output, &args.geolib, &args.separator).unwrap();
+            }
+
+        },
     }
 }
